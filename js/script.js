bd7b735e07b37478aa1c496bfe833dc43c7bb3c4
MAKONJIO99 <francismakonjio2@gmail.com>
2021-04-10 16:24:35 +0300

    add the gender fuction to confirm if the gender is checked in js

--- js/script.js
+++ js/script.js
@@ -5,4 +5,18 @@ function getAkanName () {
     let dayOfBirth = Number(document.getElementById("day-input").value);
   
     let genders = document.getElementsByName("gender");
+
+     //the gender function
+     function getGender () {
+        for (let gender of genders){
+          if (gender.checked){
+            return gender.value;
+          }
+        }
+      }
+    
+      let myGenderValue = getGender();
+    
+      console.log(myGenderValue);
+    
   
